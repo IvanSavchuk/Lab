01f1010de11a00bce9556548b88b14370c6c0fe2
IvanSavchuk <isavchuk99@gmail.com>
2019-04-17 10:04:57 +0300

    add first code to Lab 4

--- Lab_4/test.rb
+++ Lab_4/test.rb
@@ -1,1 +1,27 @@
 # Тут колись буде код! xD
+
+def mac_address
+  return @mac_address if defined? @mac_address
+  re = %r/^:-{5}[0-9A-Za-z][0-9A-Za-z][^:-]/o
+  lines = nil
+  cmds = ‘/sbin/ifconfig’, ‘/bin/ifconfig’, ‘ifconfig’,‘ipconfig /all’
+  cmds.each do |cmd|
+    begin
+      stdout = IO.popen(cmd){|fd| fd.readlines}
+    rescue
+      stdout = nil
+    end
+    next unless stdout and stdout.size > 0
+    lines = stdout and break
+  end
+  raise "failed" unless lines
+
+  candidates = lines.select{|line| line =~ re}
+  raise 'no mac address candidates' unless candidates.first
+
+  maddr = candidates.first[re]
+  raise 'no mac address found' unless maddr
+  @mac_address = maddr.strip
+end
+
+puts mac_address
