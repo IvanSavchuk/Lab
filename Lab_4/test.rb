d90db4bfb12d56c44de3fe0bca00a33b6726c108
IvanSavchuk <isavchuk99@gmail.com>
2019-05-06 20:35:41 +0300

    Refactoring code v2

--- Lab_4/test.rb
+++ Lab_4/test.rb
@@ -1,5 +1,34 @@
-# i = shell_dir_listing = `ifconfig`
+i = "------------------------------------------------------------------------------------------------------------"
+usert = "Вивести ім'я комп'ютера: "
+ipt = "Вивести IP: "
+pst = "Вивести ваші поточні активні процеси: "
+freet = "Вивести стан оперативної пам'яті в мегабайтах: "
+cput = "Вивести ЦПУ: "
 
+# ic = shell_dir_listing = `ifconfig`
+
+# Вивести ім'я комп'ютера
+user = 'hostname'
+# Вивести IP
+ip = 'ifconfig'
+# Вивести ваші поточні активні процеси
+ps = 'ps'
+# Вивести стан оперативної пам'яті в мегабайтах
+free = 'free -m'
+# Вивести ЦПУ
 cpu = 'hwinfo --cpu'
+
+print "#{usert}\n"
+system(user)
+print "#{i}\n"
+print "#{ipt}\n"
+system(ip)
+print "#{i}\n"
+print "#{pst}\n"
+system(ps)
+print "#{i}\n"
+print "#{freet}\n"
+system(free)
+print "#{i}\n"
+print "#{cput}\n"
 system(cpu)
- 
